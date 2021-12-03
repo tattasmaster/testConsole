db64ad0a5fa834ac78a66bff52e4276ed2970203
theok <theok@DESKTOP-CCDH199>
2021-12-03 10:41:48 +0100

    Ajoutez des fichiers projet.

---
+++ testConsole/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+/**
+ * Jeu du nombre caché
+ * author : Théo
+ * date : 13/30/21
+ */
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercice22
+
+            int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+            int moyenne = 0, pasMoyenne = 0;
+
+            Console.Write("Veuillez entrer 10 notes: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n3 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n4 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n5 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n6 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n7 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n8 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n9 = int.Parse(Console.ReadLine());
+            Console.Write(": ");
+            n10 = int.Parse(Console.ReadLine());
+
+            switch (n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
+            {
+                case < 10:
+                    pasMoyenne++;
+                    break;
+                case >= 10:
+                    moyenne++;
+                    break;
+            }
+
+            Console.WriteLine("Vous avez " + pasMoyenne + " notes en dessous de la moyenne et " + moyenne + " " +
+                "notes au-dessus de la moyenne ou égale(s) à la moyenne.");
+
+
+
+            Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
---
+++ testConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("testConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("f9e22912-729f-49af-bfa2-511ee03f2516")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
